bfeee6467a1d158a657cb0c649492927850aa302
Hau Nguyen <hauvipapro@gmail.com>
2023-03-25 10:54:22 +0700

    so du thang n khong de quy khong arr

--- day-02.cpp
+++ day-02.cpp
@@ -7,16 +7,20 @@ int findTopTangGiam(int* arr, int n);
 void testArrTangGiam();
 void testFindTopTangGiam();
 
+float soDuThangN(float a, float b, float c, int n);
+void testSoDuThangN();
+
 int main() {
     testArrTangGiam();
     testFindTopTangGiam();
+    testSoDuThangN();
 }
 
 // 1 3 5 4 2: ok
 // 1 1 3 5 4 2: not ok
-// idea:
-// tang dan tu dau -> stop on top
-// giam dan tu cuoi -> stop on top
+// Idea:
+// Tang dan tu dau -> stop on top
+// Giam dan tu cuoi -> stop on top
 int isArrTangGiam(int* arr, int n) {
     int indexTop = 0;
     for (int i = 0; i < n - 1; i++) {
@@ -57,7 +61,7 @@ int isArrTangGiam(int* arr, int n) {
 
 bool isArrTangGiamTeacher(int* arr, int n) {
     // 1 3 2
-    // below len 3 can not make arr tang giam
+    // Below len 3 can not make arr tang giam
     if (n < 3) {
         return false;
     }
@@ -89,8 +93,8 @@ bool isArrTangGiamTeacher(int* arr, int n) {
 }
 
 // 1 3 5 4 2 -> top is 5
-// return index top
-// because arr is tang giam, index top always exist
+// Return index top
+// Because arr is tang giam, index top always exist
 // Demo
 // 1 3 5 4 2: left = 0, right = 4
 // -> mid = 2
@@ -176,7 +180,8 @@ void testArrTangGiam() {
     printf("arr tang giam: %d\n", isArrTangGiamTeacher(arr, 0));
 }
 
-// TODO: Tim day tang giam dai nhat trong day tang giam (chi tang, chi giam, tang ngat roi giam ngat)
+// TODO: Tim day tang giam dai nhat trong day tang giam (chi tang, chi giam,
+// tang ngat roi giam ngat)
 
 void testFindTopTangGiam() {
     {
@@ -225,5 +230,50 @@ void testFindTopTangGiam() {
         arr[6] = 0;
 
         printf("top index of arr tang giam: %d\n", findTopTangGiam(arr, 7));
+    }
+}
+
+// x_0 = a
+// x_n = (1 + b) * x_(n - 1) (n mod 3 != 0)
+// x_n = (1 + b) * x_(n - 1) + c (n mod 3 == 0)
+float soDuThangN(float a, float b, float c, int n) {
+    if (n == 0) {
+        return a;
+    }
+
+    if (n % 3 != 0) {
+        return (1 + b) * soDuThangN(a, b, c, n - 1);
+    } else {
+        return (1 + b) * soDuThangN(a, b, c, n - 1) + c;
+    }
+}
+
+// Khong de quy
+// Khong su dung mang
+// Luu bien tam chay vong lap
+float soDuThangNV2(float a, float b, float c, int n) {
+    float soDu = a;
+    for (int i = 0; i < n; i++) {
+        if (n % 3 != 0) {
+            soDu = (1 + b) * soDu;
+        } else {
+            soDu = (1 + b) * soDu + c;
+        }
+    }
+
+    return soDu;
+}
+
+// Cho truoc b, c
+// Tim a toi thieu de sau k thang co so du toi thieu la M
+
+void testSoDuThangN() {
+    {
+        for (int i = 0; i <= 4; i++) {
+            printf("so du thang %d %.6f\n", i,
+                   soDuThangN(100000000, 0.01, 50000, i));
+            printf("so du thang v2 %d %.6f\n", i,
+                   soDuThangN(100000000, 0.01, 50000, i));
+        }
     }
 }

c3ee9e6fc22fe1a9b5e984d2388a4047ee81c13c
Hau Nguyen <hauvipapro@gmail.com>
2023-04-07 23:27:54 +0700

    feat: oop thiet bi dien
    virtual only work withs pointer

--- day-03.cpp
+++ day-03.cpp
@@ -2,4 +2,86 @@
 
 using namespace std;
 
-int main() {}
+class MyClass {
+  public:
+    int A;
+    void doIt() { printf("Do it\n"); }
+    // Constructor
+    MyClass() { printf("Constructor\n"); }
+};
+
+class ChildClass : public MyClass {};
+
+void test();
+
+class ThietBiDien {
+  public:
+    virtual void TurnOn() { printf("Turn on\n"); }
+};
+
+class TBDen : public ThietBiDien {
+  protected:
+    int doSang;
+};
+
+class TBDenNgu : public TBDen {
+  private:
+    int mauSang;
+
+  public:
+    void changeMauSang(int mauSang) { this->mauSang = mauSang; }
+};
+
+class TBDenChieuSang : public TBDen {};
+
+class TBDieuHoa : public ThietBiDien {
+  private:
+    int nhietDo;
+    int cheDoHoatDong;
+    int mucDoQuat;
+
+  public:
+    void changeNhietDo(int nhietDo) { this->nhietDo = nhietDo; }
+    void TurnOn() { printf("Dieu hoa turn on\n"); }
+};
+
+class TBTivi : public ThietBiDien {};
+
+void testThietBiDien();
+
+int main() {
+    test();
+    testThietBiDien();
+}
+
+void test() {
+    MyClass c;
+    c.A = 5;
+    printf("%d\n", c.A);
+    c.doIt();
+
+    ChildClass cc;
+    cc.A = 6;
+    printf("%d\n", cc.A);
+    cc.doIt();
+}
+
+void testThietBiDien() {
+    ThietBiDien* arr[10];
+
+    arr[0] = new TBDen();
+
+    arr[1] = new TBDieuHoa();
+
+    for (int i = 0; i < 2; i++) {
+        // If pointer (*abc).TurnOn is equal to abc->TurnOn
+        arr[i]->TurnOn();
+    }
+
+    // Slicing
+    // https://stackoverflow.com/a/21068741
+    // TBDieuHoa is lost, ThietBiDien only
+    // Because not pointer
+    ThietBiDien t = TBDieuHoa();
+    t.TurnOn();
+}
